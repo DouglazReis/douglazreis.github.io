65fc96464ac4982075409704d2dcf05d54c0ed1e
Douglas Reis <douglaszreisz@gmail.com>
2024-06-28 14:00:14 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Obrigado por entrar em contato, ${name}! Recebemos sua mensagem.`);
+
+    // Limpar o formulário após o envio
+    document.getElementById('contactForm').reset();
+});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
